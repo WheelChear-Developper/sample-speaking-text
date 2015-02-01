cfbe6abf35d9c2d999eaade34a232df9f00435bc
Mobile Innovation, LLC <amatani@mobile-innovation.jp>
2015-02-01 17:35:46 +0900

    no message

---
+++ README.md
@@ -0,0 +1,7 @@
+StoryTeller
+===========
+
+sample code of AVSpeechSynthesizer.
+
+Plese read this page for the usage of this code.
+http://www.toyship.org/archives/1483
---
+++ StoryTellerTests/StoryTellerTests.m
@@ -0,0 +1,57 @@
+//
+//  StoryTellerTests.m
+//  StoryTellerTests
+//
+//  Created by Tachibana Kaoru on 25/09/2013.
+//  Copyright (c) 2013 Toyship.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StoryTellerTests : XCTestCase
+
+@end
+
+@implementation StoryTellerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+	
+	sleep(10);
+	
+    [super tearDown];
+}
+
+- (void)testExample1
+{
+	XCTAssertNil(nil);
+}
+
+- (void)testExample2
+{
+	XCTAssertNil(nil);
+}
+
+- (void)testExample3
+{
+ 	XCTAssertNil(nil);
+}
+
+- (void)testExample4
+{
+ 	XCTAssertNil(nil);
+}
+
+- (void)testExample5
+{
+	XCTAssertNil(nil);
+}
+
+@end
